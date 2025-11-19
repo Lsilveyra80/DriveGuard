80024d6172963abd81c7dc570a45913229ac0cf9
Luigi1980 <47399028+Lsilveyra80@users.noreply.github.com>
2025-11-19 15:12:41 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Carga MediaPipe Tasks Vision en el navegador */}
         <script
           type="module"
           src="https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/vision_bundle.mjs"
